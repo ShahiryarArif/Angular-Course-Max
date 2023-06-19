3df9f7d3ae0162a320dfc5803be32ebb41664347
Shahiryar Arif <sarif@cinnova.com>
2023-06-19 18:16:11 +0500

    Assignment 1 solution

---
+++ basics-assignment-1-start/src/app/warningalert/warningalert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-warningalert",
+  template: "<p>WarningAlert</p>",
+  styles: ["p{color: red}"],
+})
+export class WarningAlertComponent {}
