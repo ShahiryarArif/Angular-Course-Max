e1ee12b58ceb19ec7fda0ec76dee91d98651d383
Shahiryar Arif <sarif@cinnova.com>
2023-06-22 14:19:54 +0500

    48. Created all required components structure

---
+++ course-project/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list',
+  template: '<p class="btn btn-primary">test shoping</p>',
+})
+export class ShoppingListComponent {}
