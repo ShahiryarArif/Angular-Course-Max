34aeb4951792253bc70849ef5a7a2a5ac5db86d2
Shahiryar Arif <sarif@cinnova.com>
2023-06-23 15:55:02 +0500

    64. Spliting the code

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
 })
 export class ServerElementComponent implements OnInit {
+  element: { name: string; type: string; content: string };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
